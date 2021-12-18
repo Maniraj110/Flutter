785d058fa113b4b20af82105efd864c4263e29d0
maniraj <maniraj@localhost.localdomain>
2021-12-18 04:28:54 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("RajFlix"),
+      ),
+      body: const Center(
+        child: Text("Welcome!"),
+      ),
+    );
+  }
+}
